7f7b6a130bfc0e57c8294b0f30f50cc55d82f153
Dejmasu Dorian <dejdori95@gmail.com>
2018-12-13 21:31:20 +0200

    Commit

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,13 +1,37 @@
 package org.fasttrackit;
 
-/**
- * Hello world!
- *
- */
+/** Car Creation*/
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Car car = new Car();
+        car.name = "Dacia";
+        car.color = "white";
+        car.doorCount = 4;
+        car.mileage = 7.5;
+        car.running = true;
+
+
+/** car.engine = new Engine(); - aici ai optiunea de a declara tipul de motor
+ *
+ * sau putem sa declaram tipul de motor folosind prima data clasa Engine*/
+
+
+        Engine engine = new Engine();
+        engine.manufacturer = "Renault";
+        engine.capacity = 1500;
+        car.engine = engine;
+
+        car.engine.manufacturer = "BMW";   /** engine.manufacturer ii acelasi cu car.engine.manufacturer
+
+     deci ultima valoare a lui manufacturer este BMW
+     */
+        engine = null;
+
+
+
+        Car car2 = new Car();
+        car2.name = "sfa";
     }
 }
