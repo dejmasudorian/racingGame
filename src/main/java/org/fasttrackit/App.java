04f6377e36f2404b48ccb939ceb592f806d5a23e
Dejmasu Dorian <dejdori95@gmail.com>
2018-12-20 21:26:49 +0200

    Methods and constructors. Learning.

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -10,11 +10,12 @@ public class App
         engine.capacity = 1500;
 
         Car car = new Car(engine);
-        car.name = "Dacia";
-        car.color = "white";
+        car.setName("Dacia");
+        car.setColor("White");
         car.doorCount = 4;
-        car.mileage = 7.5;
+        car.setMileage(7.5);
         car.running = true;
+        System.out.println(car.getName());
 
 
 /** car.engine = new Engine(); - aici ai optiunea de a declara tipul de motor
--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -1,14 +1,38 @@
 package org.fasttrackit;
 
 public class Vehicle {
-    String name;
-    String color;
-    double mileage;
+    private String name;
+    private String color;
+    private double mileage;
 
     protected double accelerate(double speed, double durationHours) {
         System.out.println(name + " accelerated with " + speed + " km/h " + durationHours + " hours.");
         double traveldistance = speed * durationHours;
         System.out.println("Travalled distance = "+ traveldistance);
         return traveldistance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name.trim();
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(double mileage) {
+        this.mileage = mileage;
     }
 }
