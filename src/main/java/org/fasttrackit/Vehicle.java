177861bca7595c6dd4452b395dffa09e547d2a90
Dejmasu Dorian <dejdori95@gmail.com>
2019-01-17 19:58:54 +0200

    Studied operators.

--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -8,16 +8,26 @@ public class Vehicle {
     private String name;
     private String color;
     private double mileage;
-
+    private double fuelLevel;
+    private double totalTravelDistance;
 
     public Vehicle() {
         vehiclecount++;
     }
 
     public double accelerate(double speed, double durationHours) {
+
         System.out.println(name + " accelerated with " + speed + " km/h " + durationHours + " hours.");
         double traveldistance = speed * durationHours;
         System.out.println("Travalled distance = "+ traveldistance);
+
+        totalTravelDistance += traveldistance;
+        System.out.println("Total travel distance : " + totalTravelDistance);
+
+        double spentFuel = traveldistance * mileage / 100;
+        fuelLevel -= spentFuel;
+        System.out.println("Remaining fuel :" + fuelLevel);
+
         return traveldistance;
     }
 
@@ -62,6 +72,21 @@ public class Vehicle {
         this.mileage = mileage;
     }
 
+    public double getFuelLevel() {
+        return fuelLevel;
+    }
+
+    public void setFuelLevel(double fuelLevel) {
+        this.fuelLevel = fuelLevel;
+    }
+
+    public double getTotalTravelDistance() {
+        return totalTravelDistance;
+    }
+
+    public void setTotalTravelDistance(double totalTravelDistance) {
+        this.totalTravelDistance = totalTravelDistance;
+    }
 
     @Override
     public String toString() {
